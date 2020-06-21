b32322d7bd501d02df66bc38ea5c79dcaaabdd8b
jiziye <jiziye2016@163.com>
2020-06-21 07:01:23 -0700

    add LinkedList/main.cpp

---
+++ LinkedList/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "LinkedList.h"
+
+
+using namespace std;
+
+int main()
+{
+	LinkedList<int> list;
+	for (int i = 0; i < 10; i++)
+	{
+		int item;
+		cin >> item;
+		list.insertFront(item);
+
+	}
+	cout << "list: ";
+	list.reset();
+	while (!list.endOfList()) {
+		cout << list.data() << " ";
+		list.next();
+	}
+
+	cout << endl;
+	int key;
+	cout << "enter ";
+	cin >> key;
+	list.reset();
+	while (!list.endOfList()) {
+		if (list.data() == key)
+			list.deleteCurrent();
+		list.next();
+	}
+	cout << "list: ";
+	list.reset();
+	while (!list.endOfList()) {
+		cout << list.data() << " ";
+		list.next();
+	}
+	cout << endl;
+	return 0;
+
+}
